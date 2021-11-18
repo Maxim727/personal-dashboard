d8a305f7e079d02c58f46d81b1477a29b958a4bb
Max <maximlee17@gmail.com>
2021-11-18 22:21:40 +0600

    added angular animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,45 @@
+import { animate, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAnim', [
+      transition('* => *', [
+        query(':leave', [
+          style({
+            display: 'block'
+          }),
+          animate(300, style({
+            opacity: 0
+          }))
+        ], {optional: true}),
+         
+        query(':enter', [
+        style({
+          opacity: 0,
+          display: 'block'
+        }),
+        animate(300, style({
+          opacity: 1
+        }))
+      ], {optional: true})
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'personal-dashboard';
   currentDate = new Date()
+
+  prepareRoute(outlet: RouterOutlet){
+    if (outlet.isActivated){
+      return outlet.activatedRoute.snapshot.url
+    }
+    return
+    
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,11 +18,13 @@ import { BookmarkTileComponent } from './bookmark-tile/bookmark-tile.component';
     BookmarksComponent,
     TodosComponent,
     NotesComponent,
-    BookmarkTileComponent
+    BookmarkTileComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
