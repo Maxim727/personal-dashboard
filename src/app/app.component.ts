7f71fb784cab0d56c08e168fdd30fb940dd133e4
Max <maximlee17@gmail.com>
2021-11-21 01:29:08 +0600

    added sequence to animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, query, style, transition, trigger } from '@angular/animations';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -8,25 +8,82 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('routeAnim', [
-      transition('* => *', [
-        query(':leave', [
-          style({
-            display: 'block'
-          }),
-          animate(300, style({
-            opacity: 0
-          }))
-        ], {optional: true}),
-         
-        query(':enter', [
+      transition(':increment', [
         style({
-          opacity: 0,
-          display: 'block'
+          position: 'relative',
+          overflow: 'hidden'
         }),
-        animate(300, style({
-          opacity: 1
-        }))
-      ], {optional: true})
+
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          })
+        ], { optional: true }),
+
+
+        group([
+          query(':leave', [
+            animate('300ms ease-out', style({
+              opacity: 0,
+              transform: 'translateX( -80px)'
+            }))
+          ], { optional: true }),
+
+          query(':enter', [
+            style({
+              opacity: 0,
+              transform: 'translateX( 80px)'
+            }),
+            animate(300, style({
+              opacity: 1,
+              transform: 'translateX(0 )'
+            }))
+          ], { optional: true })
+        ]),
+
+      ]),
+
+      transition(':decrement', [
+        style({
+          position: 'relative',
+          overflow: 'hidden'
+        }),
+
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          })
+        ], { optional: true }),
+
+
+        group([
+          query(':leave', [
+            animate('300ms ease-out', style({
+              opacity: 0,
+              transform: 'translateX( 80px)'
+            }))
+          ], { optional: true }),
+
+          query(':enter', [
+            style({
+              opacity: 0,
+              transform: 'translateX( -80px)'
+            }),
+            animate(300, style({
+              opacity: 1,
+              transform: 'translateX(0 )'
+            }))
+          ], { optional: true })
+        ]),
+
       ])
     ])
   ]
@@ -34,12 +91,12 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   currentDate = new Date()
 
-  prepareRoute(outlet: RouterOutlet){
-    if (outlet.isActivated){
-      return outlet.activatedRoute.snapshot.url
+  prepareRoute(outlet: RouterOutlet) {
+    if (outlet.isActivated) {
+      return outlet.activatedRouteData['tab']
     }
     return
-    
+
   }
 
 }
