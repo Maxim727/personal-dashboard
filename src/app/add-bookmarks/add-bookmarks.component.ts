3361954c1510c3e009d31d618be3326422018a58
Max <maximlee17@gmail.com>
2021-12-06 09:04:53 +0600

    add bookmarks funcitonality

---
+++ src/app/add-bookmarks/add-bookmarks.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Bookmark } from '../shared/bookmark.model';
+import { BookmarkService } from '../shared/bookmark.service';
+
+@Component({
+  selector: 'app-add-bookmarks',
+  templateUrl: './add-bookmarks.component.html',
+  styleUrls: ['./add-bookmarks.component.scss']
+})
+export class AddBookmarksComponent implements OnInit {
+  showValidationErrors!: boolean;
+
+  constructor(private router: Router, private bookmarkService: BookmarkService) { }
+
+  ngOnInit(): void {
+  }
+
+  //@ts-ignore
+  onFormSubmit(form: NgForm){
+    
+    console.log(form.invalid)
+
+    if(form.invalid) return this.showValidationErrors = true;
+
+    const bookmark = new Bookmark(form.value.name, form.value.url)
+    this.bookmarkService.addBookmark(bookmark)
+
+    this.router.navigateByUrl('/bookmarks')
+  }
+  
+}
+
